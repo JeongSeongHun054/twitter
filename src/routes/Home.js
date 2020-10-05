8221ef3731bd19d1141764dccecd6eb615a8f7fc
JeongSeongHun054 <gnsl1465@naver.com>
2020-10-05 15:42:50 +0900

    Tweet CRUD

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Nweet from "../components/Nweet";
+import { dbService } from "../fBase";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
-  const onSubmit = (e) => {
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray);
+    });
+  }, []);
+  const onSubmit = async (e) => {
     e.preventDefault();
+    await dbService.collection("nweets").add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
     setNweet("");
   };
 
@@ -15,16 +33,27 @@ const Home = () => {
   };
 
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="What's on your mind?"
-        maxLength={20}
-        value={nweet}
-        onChange={onChange}
-      />
-      <input type="submit" value="Tweet" onSubmit={onSubmit} />
-    </form>
+    <>
+      <form>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={20}
+          value={nweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="Tweet" onClick={onSubmit} />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
