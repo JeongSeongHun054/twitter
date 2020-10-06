d07d7a57263227a3d4a85a3c0fc55d004ba1b217
JeongSeongHun054 <gnsl1465@naver.com>
2020-10-06 16:50:00 +0900

    Updates

---
+++ precache-manifest.1c8a725669dd844ef3e26d948440d3c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a354f360375d15cb65bba0c94fd283",
+    "url": "/twitter/index.html"
+  },
+  {
+    "revision": "a4a7fd2724f3f573557d",
+    "url": "/twitter/static/css/main.51c00765.chunk.css"
+  },
+  {
+    "revision": "5d2da7badca18d5988ea",
+    "url": "/twitter/static/js/2.44ac9112.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/twitter/static/js/2.44ac9112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a7fd2724f3f573557d",
+    "url": "/twitter/static/js/main.11306d72.chunk.js"
+  },
+  {
+    "revision": "9f20f10883ff1630816b",
+    "url": "/twitter/static/js/runtime-main.39cb9049.js"
+  }
+]);
